bd983dd107ce04b6c28082e919a23a29081ca9c4
lee5031207 <lee5031207@naver.com>
2020-11-26 00:00:01 +0900

    2020.11.25 DML

--- B_DB/b_ddl/e_ddl.sql
+++ B_DB/b_ddl/e_ddl.sql
@@ -44,5 +44,91 @@ CREATE TABLE MY_EMPLOYEE(
     --ON UPDATE CASCADE : 부모테이블의 컬럼이 수정되면 자식 테이블도 같이 수정
     --ON DELETE CASCADE : 부모테이블의 ROW가 삭제되면 자식테이블의 행도 같이 삭제
     --ON DELETE SET NULL : 부모테이블의 ROW가 삭제되면 자식테이블의 컬럼 값을 NULL로 변경
+);  
 
-);    
+--1. AGE에 20보다 작은 값을 넣고 MARRIAGE에 Z를 넣어 
+--   도메인 무결성을 어겨보자!
+INSERT INTO MY_EMPLOYEE (ENAME, AGE, MARRIAGE, EID) VALUES ('이성욱',19,'Z','AAA');
+
+--2. NOT NULL이 지정된 컬럼에 NULL을 넣어 NULL무결성을 어겨보자
+--INSERT 문에서 값을 지정하지 않은 컬럼은 자동으로 NULL이 들어가게 된다
+--INSERT INTO MY_EMPLOYEE (AGE, MARRIAGE) VALUES (19,'Z');
+
+--3. PRIMARY KEY로 지정된 E_ID에 NULL값을 넣어서 기본키의 최소성을 확인하고
+--   중복된 값도 넣어서 기본키의 유일성을 확인
+INSERT INTO MY_EMPLOYEE (ENAME, AGE, MARRIAGE, EID) VALUES ('이성욱',21,'N','AAA');
+
+--4. HIRE_DATE에 NULL을 넣어서 DEFAULT로 지정한 값이 잘 입력되는지 확인
+
+--SELECT문을 활용해서 테이블 복제
+--CREATE TABLE COPY_EMPLOYEE
+--AS SELECT * FROM EMPLOYEE;
+--SELECT * FROM COPY_EMPLOYEE;
+--DROP TABLE COPY_EMPLOYEE;
+
+--테이블 컬럼만 복제해오기, 데이터 (X)
+--CREATE TABLE COPY_EMPLOYEE
+--AS SELECT * FROM EMPLOYEE WHERE 1=0;
+--SELECT * FROM COPY_EMPLOYEE;
+--DROP TABLE COPY_EMPLOYEE;
+
+
+-----------------------------------------------------------------------------------------------------
+--테이블 수정
+--1. 컬럼
+-- ALTER TABLE 테이블명 ADD|MODIFY|DROP (컬럼명 타입 DEFAULT 제약조건)
+
+-- 1-1. 컬럼 추가(ADD)
+ALTER TABLE MY_EMPLOYEE ADD(JOB_CODE CHAR(2));
+SELECT * FROM MY_EMPLOYEE;
+-- 1-2. 컬럼 수정(MODIFY)
+-- 컬럼의 타입은 변경 불가, 단 테이블에  데이터가 하나도 없으면 타입변경 가능
+-- 컬럼의 크기는 지금보다 큰 크기로만 변경 가능
+-- NOT NULL 제약조건을 지정할 수 있다. 단 이미 NULL인 행이 존재하면 변경 불가
+-- UNIQUE 제약조건을 지정할 수 있다. 단, 이미 중복된 데이터가 있으면 변경 불가
+ALTER TABLE MY_EMPLOYEE MODIFY(JOB_CODE CHAR(10));
+ALTER TABLE MY_EMPLOYEE MODIFY(JOB_CODE NOT NULL);
+INSERT INTO MY_EMPLOYEE (ENAME, AGE, MARRIAGE, EID, JOB_CODE) VALUES ('사보현',21,'N','BBB','J7');
+INSERT INTO MY_EMPLOYEE (ENAME, AGE, MARRIAGE, EID, JOB_CODE) VALUES ('사보현',21,'N','CCC','J7');
+ALTER TABLE MY_EMPLOYEE MODIFY(JOB_CODE UNIQUE);
+-- 1-3. 컬럼 삭제(DROP)
+ALTER TABLE MY_EMPLOYEE DROP COLUMN JOB_CODE;
+
+
+-- 기본키 추가하기, 기본키 삭제하기
+ALTER TABLE MY_EMPLOYEE ADD PRIMARY KEY(ENAME);
+
+--기존 기본키 삭제
+ALTER TABLE MY_EMPLOYEE DROP PRIMARY KEY;
+
+--제약조건 추가하기, 제약조건 삭제하기
+--MY_EMPLOYEE 테이블의 ENO컬럼에 NOT NULL 제약조건 추가
+--ALTER TABLE 테이블명 ADD CONSTRAINT 제약조건명 제약 조건 종류 (컬럼명)
+ALTER TABLE MY_EMPLOYEE ADD CONSTRAINT ENO_NULL CHECK (ENO IS NOT NULL);
+SELECT * FROM MY EMPLOYEE;
+
+--제약조건 삭제하기
+--ALTER TABLE 테이블명 DROP CONSTRAINT 제약조건명
+
+--테이블 삭제
+--DROP TABLE 테이블명
+DROP TABLE MY_EMPLOYEE;
+--DROP TABLE 테이블명 CASCADE CONSTRAINT;
+--테이블과 참조관계인 테이블의 참조 제약조건도 함께 삭제
+
+--외래키 추가
+--ALTER TABLE 테이블명 ADD CONSTRAINT 제약조건명 FOREIGN KEY(컬럼명) REFERNCES 부모테이블명(컬럼명)
+ALTER TABLE TB_BOARD ADD CONSTRAINT FK_BOARD_MEMBER
+FOREIGN KEY(USER_ID) REFERENCES TB_MEMBER(USER_ID);
+--외래키 삭제
+--ALTER TABLE 테이블명 DROP CONSTRAINT 제약조건명;
+ALTER TABLE TB_BOARD DROP CONSTRAINT FK_BOARD_MEMBER;
+
+
+--[참고]
+-- DATA DICTIONARY : 데이터 사전
+--읽기 전용으로 제공되는 테이블 또는 VIEW의 집합, 데이터베이스 전반에 대한 정보를 제공
+--사용자 정보, 권한 정보, 제약 조건, 객체정보 등등
+-- 제약조건에 대한 정보를 담고 있는 DICTIONARY 조회
+SELECT * FROM ALL_CONSTRAINTS WHERE TABLE_NAME = 'TB_BOARD';
+
---
+++ B_DB/c_dml/dml.sql
@@ -0,0 +1,118 @@
+--DML(DATA MANIPULATION LANGUAGE) : 데이터 조작 언어
+-- 테이블에 값을 삽입(INSERT) 하거나 수정(UPDATE) 하거나, 삭제(DELETE)하는 것
+
+--TCL(TRANSACTION CONTROL LANGUAGE)
+--TRANSACTION : 논리적 최소 작업 단위
+--COMMIT/ROLLBACCK
+-- COMMIT : SMML에 의해서 변경된 정보를 테이블에 반영
+-- ROLLBACK : DML에 의해서 변경된 정보를 마지막 COMMIT한 시점으로 되돌려 놓는 것
+-- 주의 ! : DDL은 자동 COMMIT이다
+--           DML을 작성하고 DDL을연달아 작설할 경우 DDL 이 자동으로 COMMIT 되면서
+--           DML 도 같이 COMMIT 되어버린다.
+
+--INSERT문
+--새로운 행(ROW)를 추가하는 구문
+--표현식 : INSERT INTO 테이블명(컬럼명,컬럼명...) VALUES(데이터1,데이터2...)
+--지정하지 않은 컬럼은 NULL이 들어간다
+--표현식2 : INSERT INTO 테이블명 VALUES(데이터1, 데이터2 ... ) ->모든 컬럼에 데이터를 넣어야 한다
+-- 읽는 사람을 위해 표현식2는 사용하지 말자 !
+
+--JOB_CODE가 'J10'이고 직급명이 이사인 데이터를 JOB테이블에 추가하시오
+INSERT INTO JOB(JOB_CODE, JOB_NAME) VALUES ('J0','이사');
+SELECT * FROM JOB;
+
+--LOCATION테이블에 LOCAL_CODE가 'L6', NATIONAL_CODE가 'ID', LOCAL_NAME이 'ASIA4'인
+-- 데이터를 추가하고 COMMIT 하시오
+INSERT INTO LOCATION(LOCAL_CODE, NATIONAL_CODE, LOCAL_NAME)
+VALUES ('L6', 'ID', 'ASIA4');
+SELECT * FROM LOCATION;
+
+--서브쿼리로 데이터 입력하기
+CREATE TABLE EMP_DEPT(
+    EMP_ID NUMBER,
+    EMP_NAME VARCHAR2(30),
+    DEPT_TITLE VARCHAR2(20)
+);
+INSERT INTO EMP_DEPT(
+    SELECT EMP_ID, EMP_NAME, DEPT_TITLE
+    FROM EMPLOYEE
+    LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+);
+
+SELECT * FROM EMP_DEPT;
+
+--INSERT ALL
+-- 두 개 이상의 테이블에 한번에 데이터를 추가하는 구문
+-- 1. 서브쿼리를 사용해서 INSERT를 작성하고
+-- 2. 서브쿼리의 조건절이 동일해야 한다.
+
+--INSERT ALL로 새롭게 데이터를 추가하기 위해 EMP_DEPT테이블의 데이터를 삭제
+DELETE FROM EMP_DEPT;
+
+--새로운 테이블 생성
+CREATE TABLE EMP_DEPT_D1
+AS SELECT EMP_ID, EMP_NAME, DEPT_CODE, HIRE_DATE
+FROM EMPLOYEE WHERE 1 = 0;
+
+SELECT * FROM EMP_DEPT_D1;
+
+--EMP_DEPT 테이블에 부서코드가 D1인 직원을 조회해서
+-- 사번, 이름, 소속부서명을 추가하고
+--EMP_DEPT_D1 테이블에 부서코드가 D1인 직원을 조회해서
+--사번, 이름, 소속부서코드, 입사일을 추가
+INSERT ALL
+    INTO EMP_DEPT VALUES(EMP_ID, EMP_NAME, DEPT_TITLE)
+    INTO EMP_DEPT_D1 VALUES(EMP_ID, EMP_NAME, DEPT_CODE, HIRE_DATE)
+    SELECT EMP_ID, EMP_NAME, DEPT_TITLE, DEPT_CODE, HIRE_DATE
+    FROM EMPLOYEE E
+    LEFT JOIN DEPARTMENT D ON(E.DEPT_CODE = D.DEPT_ID)
+    WHERE DEPT_CODE = 'D1';
+    
+SELECT * FROM EMP_DEPT;
+SELECT * FROM EMP_DEPT_D1;
+
+--2.UPDATE
+-- 테이블에 기록된 컬럼 값을 수정하는 구문
+-- 표현식 
+--UPDATE 테이블명 SET 컬럼명 = 변경할 값 [WHERE 절]
+-- EMP_DEPT 테이블에서 이름이 전지연인 사원의 부서명을 ' 인사인사부' 로 변경
+UPDATE EMP_DEPT SET DEPT_TITLE = '인사인사부' WHERE EMP_NAME = '전지연' ;
+SELECT * FROM EMP_DEPT;
+COMMIT; 
+
+--UPDATE절 주의사항
+--1. 만약 WHERE절을 작성하지 않고 UPDATE문을 작성할 경우 해당 테이블의 모든 행이 수정되기 때문에
+--   반드시 WHERE절을 작성할 것!!!
+--2. UPDATE문을 작성하고 실행하기 전에 반드시 같은 WHERE절을 가진 SELECT문을 먼저
+--    실행해 변경하고자 하는 데이터가 맞는 지 확인하고 UPDATE문을 실행할 것
+
+--서브쿼리를 이용한 UPDATE 문
+-- 표현식 : UPDATE 테이블명 SET 컬럼명 = (서브쿼리)
+-- 방명수 사원의 급여와 보너스율을 유재식 사원과 동일하게 변경해주는 UPDATE문
+UPDATE EMPLOYEE
+SET SALARY = (SELECT SALARY FROM EMPLOYEE WHERE EMP_NAME = '유재식'),
+    BONUS = (SELECT BONUS FROM EMPLOYEE WHERE EMP_NAME = '유재식')
+WHERE EMP_NAME = '방명수';
+
+SELECT * FROM EMPLOYEE WHERE EMP_NAME IN('유재식','방명수');
+ROLLBACK;
+
+--아시아 지역에서 근무하는 직원의 보너스를 0.3으로 조정하시오
+UPDATE EMPLOYEE 
+SET BONUS = 0.3
+WHERE DEPT_CODE = (
+SELECT DEPT_ID  FROM DEPARTMENT D
+JOIN LOCATION L ON(D.LOCATION_ID = L.LOCAL_CODE)
+WHERE LOCAL_NAME LIKE '%ASIA%'
+);
+
+SELECT * FROM EMPLOYEE;
+
+SELECT E.DEPT_CODE FROM EMPLOYEE E, DEPARTMENT D, LOCATION L
+WHERE E.DEPT_CODE = D.DEPT_ID
+AND D.LOCATION_ID = L.LOCAL_CODE 
+AND L.LOCAL_NAME LIKE '%ASIA%';
+
+SELECT  *  FROM DEPARTMENT D
+JOIN LOCATION L ON(D.LOCATION_ID = L.LOCAL_CODE)
+WHERE LOCAL_NAME LIKE '%ASIA%';
