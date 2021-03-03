49ab663e893462a20ca586c1921830932cb5198b
lee5031207 <lee5031207@naver.com>
2021-03-03 21:56:58 +0900

    2021.03.03 three.js 시작

---
+++ A_JAVA/i_codingTest/src/level_1/폰켓몬.java
@@ -0,0 +1,27 @@
+package level_1;
+
+public class 폰켓몬 {
+
+	public static void main(String[] args) {
+		int[] nums = {3,1,2,3};
+		solution(nums);
+	}
+	
+    public static int solution(int[] nums) {
+        int answer = 0;
+        int n = nums.length/2;
+        
+        // 4개면은 6개의 조합
+        // 6개면은 
+        // 
+        
+
+
+        
+        
+        return answer;
+    }
+	
+	
+
+}
